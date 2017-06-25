c621da5568185fae46153e39893f8c59fb4d019d
Lawrence Carvalho <lawrencec@users.noreply.github.com>
2017-06-25 14:56:38 +0100

    Fixes #21 - fixes bug where async test functions would always pass (#26)

--- examples/ava/ava-example.js
+++ examples/ava/ava-example.js
@@ -58,3 +58,44 @@ unroll(
     /* change last entry to [7, 0, 0] to see failure */
   ]
 );
+
+unroll.use(test);
+
+unroll('[ava] async/await calculates the maximum of #b and #a',
+    async function (t, testArgs) {
+      const testPromise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          resolve(Math.max(testArgs.a, testArgs.b));
+        }, 200);
+      });
+      const result = await testPromise;
+      t.plan(1);
+      t.is(result, testArgs.c);
+    },
+  [
+      ['a', 'b', 'c'],
+      [3, 5, 5],
+      [7, 0, 7]
+      /* change last entry to [7, 0, 0] to see failure */
+  ]
+);
+
+unroll('[ava] async/await passes errors correctly when the maximum of #b and #a is not equal to #c',
+    async function (t, testArgs) {
+      const testPromise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          reject('Uh-oh');
+        }, 200);
+      });
+
+      return await testPromise
+          .catch(function (err) {
+            t.is(err, 'Uh-oh');
+          });
+    },
+  [
+      ['a', 'b', 'c'],
+      [3, 5, 3],
+      [7, 0, 0]
+  ]
+);
